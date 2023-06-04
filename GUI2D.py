c342554a85052d55bffb1fa6cb05765563445c77
Zebaro24-Stud <zebaro24@stu.cn.ua>
2023-06-04 23:52:07 +0300

    Додано GUI2D, LabyrinthLogic, utils - Заготовки для створення методів в них

---
+++ GUI2D.py
@@ -0,0 +1,8 @@
+from utils import set_logger
+import pygame
+import random
+
+
+class GUI2D:
+    def __init__(self):
+        pass
---
+++ LabyrinthLogic.py
@@ -0,0 +1,7 @@
+from utils import set_logger
+
+
+class LabyrinthLogic:
+
+    def __init__(self):
+        pass
