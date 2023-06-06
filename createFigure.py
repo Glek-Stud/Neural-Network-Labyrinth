9d96dd01cf6d3308bde0a252ecc6e8a7ffd2e237
WhiteDh <100050383+WhiteDh@users.noreply.github.com>
2023-06-06 03:16:38 +0300

    додано деякі коментарі

--- createFigure.py
+++ createFigure.py
@@ -1,6 +1,1 @@
-import pygame
 
-def createButton(x1, y1, height, high):
-
-    button_rect = pygame.Rect(x1, y1, height, high)
-    return button_rect
--- window.py
+++ window.py
@@ -1,9 +1,12 @@
 import pygame
 from pygame.locals import *
-import createFigure
 
+# initialisation of pygame
 pygame.init()
+
+
 # size of window
+
 # WIDTH = 1920
 # HEIGHT = 1080
 
@@ -18,32 +21,51 @@ HEIGHT = screen_info.current_h
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
-# Создание класса окна
+
+
+# creating button
+button = pygame.Rect(800, 500, 50, 20)
+
+
+# creating of class of window
 class Window:
+
     def __init__(self):
+        # create window
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+        # name of window
         pygame.display.set_caption("Labyrinth")
+
+        # for fps
         self.clock = pygame.time.Clock()
-        self.button = createFigure.createButton(800, 500, 50, 20)
+
+
 
 
     def run(self):
         
         running = True
         while running:
+            # to detect some events
             for event in pygame.event.get():
                 if event.type == QUIT:
                     running = False
-                elif event.type == MOUSEBUTTONDOWN and event.button == 1:
-                    if self.button.collidepoint(event.pos):
-                        print("click))))")
 
+                # if was clicked
+                elif event.type == MOUSEBUTTONDOWN and event.button == 1:
+                    if button.collidepoint(event.pos):
+                        print("click))))")
             self.screen.fill(WHITE)
 
             # some code here
-            # example from createFigure.py
-            pygame.draw.rect(self.screen, BLACK, self.button)
+            # example
+            pygame.draw.rect(self.screen, BLACK, button)
+
+
+
             pygame.display.update()
+            # fps on screen
             self.clock.tick(60)
 
 
