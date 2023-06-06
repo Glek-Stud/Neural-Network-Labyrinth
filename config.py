87886eeba2b286a8b199e5d9b9d921b622f409e5
Zebaro24-Stud <zebaro24@stu.cn.ua>
2023-06-06 18:35:37 +0300

    Додано config - Для збереження констант

---
+++ config.py
@@ -0,0 +1,15 @@
+# <Window>
+# size of window
+
+# WIDTH = 800 <Автоматично>
+# HEIGHT - Всі компоненти
+SIZE_LAB = 600
+SIZE_BUTTON = 200
+HEIGHT = SIZE_LAB + SIZE_BUTTON
+
+# colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+# size of block
+BLOCKSIZE = 22
