7d722bfb3fda2afe02af7e14f78e112c400a78ae
WhiteDh <100050383+WhiteDh@users.noreply.github.com>
2023-06-08 03:11:56 +0300

    прибрав змінні, що не використовувалися

--- GUI2D.py
+++ GUI2D.py
@@ -1,15 +1,15 @@
 from utils import set_logger
 # import random
 import pygame
-from pygame.locals import QUIT, KEYDOWN, K_u, MOUSEMOTION, MOUSEBUTTONDOWN, MOUSEBUTTONUP
+from pygame.locals import QUIT, MOUSEMOTION, MOUSEBUTTONDOWN, MOUSEBUTTONUP
 from LabyrinthLogic import LabyrinthLogic
 from config import HEIGHT, WIDTH, WHITE, BLACK, BLOCKSIZE, FRAME, GRAY, TITLE, DARK_GRAY
-
 
 
 class GUI2D:
     # initialisation of pygame
     pygame.init()
+
     def __init__(self):
         self.logger = set_logger("GUI 2D")
         self.labyrinth_class = LabyrinthLogic(25)
@@ -17,7 +17,7 @@ class GUI2D:
         self.labyrinth = self.labyrinth_class.get_border_labyrinth()
 
         # create window
-        self.screen = pygame.display.set_mode((WIDTH, HEIGHT ), flags = pygame.NOFRAME | pygame.DOUBLEBUF)
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT), flags=pygame.NOFRAME | pygame.DOUBLEBUF)
 
         # name of window
         pygame.display.set_caption("Labyrinth")
@@ -25,29 +25,24 @@ class GUI2D:
         # for fps
         self.clock = pygame.time.Clock()
 
-
-        #resizing frame
+        # resizing frame
         self.originSize = 5
         self.hoveredSize = 35
         self.currentSize = self.originSize
 
-
-        #detect mouse on panel
+        # detect mouse on panel
         self.detect = False
 
-
-        #title
+        # title
         self.font = pygame.font.Font(None, 36)
         self.text_surface = self.font.render(TITLE, True, WHITE)
         self.rotated_surface = pygame.transform.rotate(self.text_surface, -90)
-
-
 
         # colors for buttons
         self.colorOfButtonClose = GRAY
         self.colorOfButtonHide = GRAY
 
-        #states of buttons
+        # states of buttons
         self.closeButtonState = "normal"
         self.hideButtonState = "normal"
 
@@ -55,10 +50,10 @@ class GUI2D:
         # draw labyrinth by random matrix
         for row in range(len(self.labyrinth)):
             for col in range(len(self.labyrinth[row])):
-                x = (WIDTH - len(self.labyrinth) * BLOCKSIZE) / 2 + col * BLOCKSIZE
-                y = (HEIGHT + FRAME - len(self.labyrinth) * BLOCKSIZE) / 2 + row * BLOCKSIZE
+                x = (WIDTH + FRAME - len(self.labyrinth) * BLOCKSIZE) / 2 + col * BLOCKSIZE
+                y = (HEIGHT - len(self.labyrinth) * BLOCKSIZE) / 2 + row * BLOCKSIZE
 
-                pygame.draw.rect(self.screen, GRAY, (WIDTH-5, 0, 10, HEIGHT), border_radius=40)
+                pygame.draw.rect(self.screen, GRAY, (WIDTH - 5, 0, 10, HEIGHT), border_radius=40)
                 if self.labyrinth[row][col] == 1:
                     pygame.draw.rect(self.screen, BLACK, (x, y, BLOCKSIZE, BLOCKSIZE))
                 else:
@@ -77,19 +72,19 @@ class GUI2D:
                 if event.type == MOUSEMOTION:
                     mouse_x, mouse_y = event.pos
 
-                    #if mouse is on panel
-                    if WIDTH-self.currentSize <= mouse_x <= WIDTH and 0 <= mouse_y <= HEIGHT:
+                    # if mouse is on panel
+                    if WIDTH - self.currentSize <= mouse_x <= WIDTH and 0 <= mouse_y <= HEIGHT:
                         self.currentSize = self.hoveredSize
                         self.detect = True
 
-                        #if mouse is on close button
+                        # if mouse is on close button
                         if WIDTH - 35 <= mouse_x <= WIDTH and 20 <= mouse_y <= 55:
                             self.colorOfButtonClose = DARK_GRAY
                             self.closeButtonState = True
                         else:
                             self.colorOfButtonClose = GRAY
 
-                        #if mouse is on hide button
+                        # if mouse is on hide button
                         if WIDTH - 35 <= mouse_x <= WIDTH and 55 <= mouse_y <= 90:
                             self.colorOfButtonHide = DARK_GRAY
                             self.hideButtonState = True
@@ -126,11 +121,11 @@ class GUI2D:
             self.draw()
 
             # draw panel
-            pygame.draw.rect(self.screen, GRAY, (WIDTH - self.currentSize, -2, 10+self.currentSize, HEIGHT+4), border_radius=40)
+            pygame.draw.rect(self.screen, GRAY, (WIDTH - self.currentSize, -2, 10 + self.currentSize, HEIGHT + 4),
+                             border_radius=40)
 
             # if mouse on panel
             if self.detect:
-
                 # type title
                 self.screen.blit(self.rotated_surface, (WIDTH - 30, 250))
 
@@ -139,16 +134,12 @@ class GUI2D:
                 # button close
                 pygame.draw.rect(self.screen, self.colorOfButtonClose, (WIDTH - 35, 20, 35, 35))
 
-                pygame.draw.line(self.screen, (255, 255, 255), (WIDTH - 30, 25), (WIDTH-5, 50), 2)
-                pygame.draw.line(self.screen, (255, 255, 255), (WIDTH - 30, 50), (WIDTH-5, 25), 2)
-
+                pygame.draw.line(self.screen, (255, 255, 255), (WIDTH - 30, 25), (WIDTH - 5, 50), 2)
+                pygame.draw.line(self.screen, (255, 255, 255), (WIDTH - 30, 50), (WIDTH - 5, 25), 2)
 
                 # button hide
                 pygame.draw.rect(self.screen, self.colorOfButtonHide, (WIDTH - 35, 55, 35, 35))
-                pygame.draw.line(self.screen, (255, 255, 255), (WIDTH - 30, 73), (WIDTH-5, 73), 1)
-
-
-
+                pygame.draw.line(self.screen, (255, 255, 255), (WIDTH - 30, 73), (WIDTH - 5, 73), 1)
 
             pygame.display.update()
             # fps on screen
--- config.py
+++ config.py
@@ -10,6 +10,8 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GRAY = (100,100,100)
 DARK_GRAY = (50,50,50)
+
+
 # size of block
 BLOCKSIZE = 22
 
